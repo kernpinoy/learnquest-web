09cb4d97fa697a052d3e80803571a61b170c548d
Kern Jacobo <jacobokern5@gmail.com>
2024-07-13 00:04:26 +0800

    feat: added theme switching

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "~/lib/utils";
+import { ThemeProvider } from "~/components/theme-provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -15,7 +16,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
