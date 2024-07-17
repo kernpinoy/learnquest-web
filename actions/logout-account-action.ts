1696fe92199f7213350a24d0e4c9f9a49e0596bc
Kern Jacobo <jacobokern5@gmail.com>
2024-07-17 09:22:50 +0800

    add: zod schema for register

--- actions/logout-account-action.ts
+++ actions/logout-account-action.ts
@@ -6,20 +6,28 @@ import { cookies } from "next/headers";
 import { validateRequest } from "~/lib/validate-request";
 
 export default async function logoutAccountAction() {
+  // check if session is present
   const { session } = await validateRequest();
 
+  // FIXME: logic is ok, but code not
   if (!session) {
     console.log("IllegalIllegal");
     return;
   }
 
+  // Invalidate session
   await lucia.invalidateSession(session?.id);
 
+  // Create blank cookie
   const sessionCookie = lucia.createBlankSessionCookie();
+
+  // Share the cookie to browser
   cookies().set(
     sessionCookie.name,
     sessionCookie.value,
     sessionCookie.attributes
   );
+
+  // Redirect to login page
   return redirect("/");
 }
---
+++ lib/forms/register-form.ts
@@ -0,0 +1,45 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+// Teacher register form schema
+export const registerFormSchema = z
+  .object({
+    firstName: z
+      .string()
+      .min(1, "First name must not be empty.")
+      .transform((str) => str.trim()),
+    middleName: z
+      .string()
+      .min(1, "Middle name must not be empty.")
+      .transform((str) => str.trim()),
+    lastName: z
+      .string()
+      .min(1, "Last name must not be empty.")
+      .transform((str) => str.trim()),
+    username: z
+      .string()
+      .min(1, "Username must not be empty.")
+      .transform((str) => str.trim()),
+    password: z.string().min(1, "Password must not be empty."),
+    confirmPassword: z.string().min(1, "Password must not be empty."),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match.",
+    path: ["confirmPassword"],
+  });
+
+// Infer type of registration form
+export type RegisterFormSchemaType = z.infer<typeof registerFormSchema>;
+
+// Export resolver
+export const resolver = zodResolver(registerFormSchema);
+
+// Export default values
+export const defaultValues: RegisterFormSchemaType = {
+  firstName: "",
+  middleName: "",
+  lastName: "",
+  username: "",
+  password: "",
+  confirmPassword: "",
+};
